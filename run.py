d900aedc95727df23de4f49396041fc99211805e
Faye Huang <faye1821@gmail.com>
2021-02-04 15:58:57 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 import os
-print(os.environ["mongodb_url"])
+import pymongo
+
+client = pymongo.MongoClient(os.environ["mongodb_url"])
+db = client.dingdong
+products = db.products
+target_product = products.find_one()
+print(target_product)
